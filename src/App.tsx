679c31a1c9b00cf8804d3d557f2ccd1736a2fb11
Kavishka-Prabashara <kavishkaprabashara@gmail.com>
2025-05-15 19:04:34 +0530

    switch to HashRouter for GitHub Pages compatibility

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
 import './App.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import Login from './Login';
 import TaskWorkPlace from './TaskWorkPlace.tsx';
 
 function App() {
     return (
-        <BrowserRouter>
+        <HashRouter>
             <Routes>
                 <Route path="/" element={<Login />} />
                 <Route path="/taskworkface" element={<TaskWorkPlace />} />
             </Routes>
-        </BrowserRouter>
+        </HashRouter>
     );
 }
 
